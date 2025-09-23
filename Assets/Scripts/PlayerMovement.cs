3a0e4e253937b7e5655dba056a2ad8d7a3fc97bb
Sören Irlenbusch <176750049+BlackPhoenix-21@users.noreply.github.com>
2025-09-23 10:26:22 +0200

    Basic PlayerMovement und GravityField

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,9 +1,13 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rbody;
-    private int force = 250;
+    private float force = 1f;
+    private float rotationSpeed = 550f;
+    private float currentRotationInput = 0f;
+    private float approachRotation = 2f;
 
     void Start()
     {
@@ -12,17 +16,27 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        float targetRotationInput = 0f;
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward * 15f);
+            targetRotationInput = 1f;
         }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            targetRotationInput = -1f;
+        }
+
+        currentRotationInput = Mathf.Lerp(currentRotationInput, targetRotationInput, approachRotation * Time.deltaTime);
+
+        if (Mathf.Abs(currentRotationInput) > 0.01f)
+        {
+            transform.Rotate(Vector3.forward * rotationSpeed * currentRotationInput * Time.deltaTime);
+        }
+
         if (Input.GetKey(KeyCode.S))
         {
-            rbody.AddForce(Vector2.up * force);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(Vector3.forward * -15f);
+            Vector2 moveDirection = transform.up;
+            rbody.AddForce(moveDirection * force);
         }
     }
 }
