8e6eb5f13d62bd166b2b0bf85ee8e85661b076d3
Sören Irlenbusch <176750049+BlackPhoenix-21@users.noreply.github.com>
2025-09-25 14:05:30 +0200

    MainMenu Animation

---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GameObject.FindWithTag("Player").GetComponent<Animator>();
+    }
+
+    public void Animation(int index)
+    {
+        switch (index)
+        {
+            case 0:
+                anim.SetBool("isLeft", true);
+                break;
+            case 1:
+                anim.SetBool("isUp", true);
+                break;
+            case 2:
+                anim.SetBool("isRight", true);
+                break;
+            case 3:
+                anim.SetBool("isLeft", false);
+                break;
+            case 4:
+                anim.SetBool("isUp", false);
+                break;
+            case 5:
+                anim.SetBool("isRight", false);
+                break;
+        }
+    }
+
+}
