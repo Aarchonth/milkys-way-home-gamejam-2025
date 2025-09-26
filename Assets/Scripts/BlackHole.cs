51c1a93e5a518fa6fbc48e41d85350312c039b4b
Sören Irlenbusch <176750049+BlackPhoenix-21@users.noreply.github.com>
2025-09-26 17:02:49 +0200

    BugFixen

--- Assets/Scripts/BlackHole.cs
+++ Assets/Scripts/BlackHole.cs
@@ -3,11 +3,26 @@ using UnityEngine;
 public class BlackHole : MonoBehaviour
 {
     public bool isActive = true;
-    public float speed = 0.5f;
+    private float minSpeed = 2f;
+    private float speed;
+    private Transform player;
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+    }
 
     void Update()
     {
         if (!isActive) return;
         transform.position += Vector3.down * Time.deltaTime * speed;
+
+        float dir = transform.position.y - player.position.y;
+        Debug.Log(dir);
+
+        if (dir > 30)
+            speed += minSpeed;
+        else if (dir < 20)
+            speed = minSpeed;
     }
 }
